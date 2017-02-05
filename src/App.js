08db05bacebc758e7d291f95583687465a3cc783
Andreas Buehler <andreasbuehler@me.com>
2017-02-05 01:42:36 +0100

    Router Integration- Routing over history.pushState()

--- src/App.js
+++ src/App.js
@@ -2,67 +2,79 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {TodoForm, TodoList} from './components/todo';
-import {addTodo, generateId, findById, toggleTodo, updateTodo} from './lib/todoHelpers';
+import {TodoForm, TodoList, Footer} from './components/todo';
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers';
+import {pipe, partial} from './lib/utils';
 
 class App extends Component {
-    state = {
-      todos: [
-        {id: 1, name: 'Learn JSX', isComplete: true},
-        {id: 2, name: 'Build an Awsome App', isComplete: false},
-        {id: 3, name: 'ship it', isComplete: false},
-      ],
-      currentTodo:''
-    }
+	state = {
+		todos: [
+			{id: 1, name: 'Learn JSX', isComplete: true},
+			{id: 2, name: 'Build an Awsome App', isComplete: false},
+			{id: 3, name: 'ship it', isComplete: false},
+		],
+		currentTodo:''
+	}
 
-  handleToggle = (id) => {
-    const todo = findById (id, this.state.todos);
-    const toggled = toggleTodo(todo);
-    const updatedTodos = updateTodo(this.state.todos, toggled)
-    this.setState({todos: updatedTodos});
-  }
+	handleToggle = (id) => {
+		const getUpdatedTodos = pipe(findById, toggleTodo, partial(this.state.todos, updateTodo));
+		// const todo = findById (id, this.state.todos);
+		// const toggled = toggleTodo(todo);
+		// const updatedTodos = updateTodo(this.state.todos, toggled)
+		const updatedTodos = getUpdatedTodos(id, this.state.todos)
+		this.setState({todos: updatedTodos});
+	}
 
-  handleInputChange = (evt) => {
-    this.setState({
-      currentTodo: evt.target.value
-    })
-    
-  }
-  handleSubmit = (evt) => {
-    evt.preventDefault();
-    const newId = generateId();
-    const newTodo = {id: newId, name: this.state.currentTodo, isComplete:false}
-    const updatedTodos = addTodo(this.state.todos, newTodo);
-    this.setState({
-      todos:updatedTodos,
-      currentTodo:'',
-      errorMessage:''
-    })
-  }
+	handleInputChange = (evt) => {
+		this.setState({
+			currentTodo: evt.target.value
+		})
 
-  handleEmptySubmit = (evt) => {
-    evt.preventDefault();
-    this.setState({errorMessage:'Please supply a todo name'})
-  }
+	}
+	handleSubmit = (evt) => {
+		evt.preventDefault();
+		const newId = generateId();
+		const newTodo = {id: newId, name: this.state.currentTodo, isComplete:false}
+		const updatedTodos = addTodo(this.state.todos, newTodo);
+		this.setState({
+			todos:updatedTodos,
+			currentTodo:'',
+			errorMessage:''
+		})
+	}
 
-  render() {
-    const submitHandler = this.state.currentTodo ?  this.handleSubmit : this.handleEmptySubmit 
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Todos</h2>
-          </div>
-            <div className="Todo-App">
-            {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
-              <TodoForm handleInputChange={this.handleInputChange}
-                currentTodo={this.state.currentTodo} 
-                handleSubmit={submitHandler} />
-              <TodoList handleToggle={this.handleToggle} todos={this.state.todos}/>
-            </div>
-      </div>
-    );
-  }
-}
+	handleEmptySubmit = (evt) => {
+		evt.preventDefault();
+		this.setState({errorMessage:'Please supply a todo name'})
+	}
 
-export default App;
+	handleRemove = (id, evt) => {
+		evt.preventDefault();
+		const updatedTodos = removeTodo(this.state.todos, id);
+		this.setState({todos: updatedTodos});
+	}
+
+	render() {
+		const submitHandler = this.state.currentTodo ?  this.handleSubmit : this.handleEmptySubmit
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>React Todos</h2>
+				</div>
+				<div className="Todo-App">
+					{this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
+					<TodoForm handleInputChange={this.handleInputChange}
+						currentTodo={this.state.currentTodo}
+						handleSubmit={submitHandler} />
+						<TodoList handleRemove={this.handleRemove}
+							handleToggle={this.handleToggle}
+							todos={this.state.todos}/>
+							<Footer/>
+						</div>
+					</div>
+				);
+			}
+		}
+
+		export default App;
--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,7 +1,7 @@
-import {addTodo, findById, toggleTodo, updateTodo} from './todoHelpers';
+import {addTodo, findById, toggleTodo, updateTodo, removeTodo} from './todoHelpers';
 
 test('addTodo should add the passed todo to the list', () => {
-	
+
 	const startTodos = [
 		{id:1, name:'one', isComplete: false},
 		{id:2, name:'two', isComplete: false},
@@ -20,7 +20,7 @@ test('addTodo should add the passed todo to the list', () => {
 })
 
 test('addTodo should not mutate the existing todo array', () => {
-	
+
 	const startTodos = [
 		{id:1, name:'one', isComplete: false},
 		{id:2, name:'two', isComplete: false},
@@ -39,7 +39,7 @@ test('addTodo should not mutate the existing todo array', () => {
 })
 
 test('findById schould return the expected item from an array', () => {
-	
+
 	const startTodos = [
 		{id:1, name:'one', isComplete: false},
 		{id:2, name:'two', isComplete: false},
@@ -55,7 +55,7 @@ test('findById schould return the expected item from an array', () => {
 })
 
 test('findById schould return the expected item from an array', () => {
-	
+
 	const startTodo =
 		{id:2, name:'two', isComplete: false}
 	const expected =
@@ -67,7 +67,7 @@ test('findById schould return the expected item from an array', () => {
 })
 
 test('toggleTodo should not mutate the original todo', () => {
-	
+
 	const startTodo =
 		{id:2, name:'two', isComplete: false}
 
@@ -77,7 +77,7 @@ test('toggleTodo should not mutate the original todo', () => {
 })
 
 test('updateTodo should update an item by id', () => {
-	
+
 	const startTodos = [
 		{id:1, name:'one', isComplete: false},
 		{id:2, name:'two', isComplete: false},
@@ -95,7 +95,7 @@ test('updateTodo should update an item by id', () => {
 })
 
 test('updateTodo should not mutate the original array', () => {
-	
+
 	const startTodos = [
 		{id:1, name:'one', isComplete: false},
 		{id:2, name:'two', isComplete: false},
@@ -104,6 +104,38 @@ test('updateTodo should not mutate the original array', () => {
 	const updatedTodo = {id:2, name:'two', isComplete: true};
 
 	const result = updateTodo(startTodos, updatedTodo)
+
+	expect(result).not.toBe(startTodos);
+})
+
+test('removeTodo should remove an item by id', () => {
+	const startTodos = [
+		{id:1, name:'one', isComplete: false},
+		{id:2, name:'two', isComplete: false},
+		{id:3, name:'three', isComplete: false}
+	]
+	const targetId = 2;
+	const expectedTodos = [
+		{id:1, name:'one', isComplete: false},
+		{id:3, name:'three', isComplete: false}
+	]
+	const result = removeTodo(startTodos, targetId);
+
+	expect(result).toEqual(expectedTodos);
+})
+
+test('removeTodo should not mutate the original array', () => {
+	const startTodos = [
+		{id:1, name:'one', isComplete: false},
+		{id:2, name:'two', isComplete: false},
+		{id:3, name:'three', isComplete: false}
+	]
+	const targetId = 2;
+	const expectedTodos = [
+		{id:1, name:'one', isComplete: false},
+		{id:3, name:'three', isComplete: false}
+	]
+	const result = removeTodo(startTodos, targetId);
 
 	expect(result).not.toBe(startTodos);
 })
