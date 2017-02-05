65a319bd10ad5b0ecd2b8f8b4410849a9ac85c9f
Andreas Buehler <andreasbuehler@me.com>
2017-02-05 03:29:41 +0100

    added deleteTodo to the Api- App finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo';
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
-import {loadTodos, createTodo} from './lib/todoService';
+import {loadTodos, createTodo, saveTodo, destroyTodo} from './lib/todoService';
 
 class App extends Component {
 	state = {
@@ -22,12 +22,13 @@ class App extends Component {
 			.then(todos => this.setState({todos}))
 	}
 	handleToggle = (id) => {
-		const getUpdatedTodos = pipe(findById, toggleTodo, partial(this.state.todos, updateTodo));
-		// const todo = findById (id, this.state.todos);
-		// const toggled = toggleTodo(todo);
-		// const updatedTodos = updateTodo(this.state.todos, toggled)
-		const updatedTodos = getUpdatedTodos(id, this.state.todos)
-		this.setState({todos: updatedTodos});
+		const getToggledTodo = pipe(findById, toggleTodo)
+		const updated = getToggledTodo(id, this.state.todos)
+		const getUpdatedTodos = partial(updateTodo, this.state.todos)
+		const updatedTodos= getUpdatedTodos(updated);
+		this.setState({todos: updatedTodos})
+		saveTodo(updated)
+			.then(() => this.showTempMessage('Todo updated'))
 	}
 
 	handleInputChange = (evt) => {
@@ -64,6 +65,8 @@ class App extends Component {
 		evt.preventDefault();
 		const updatedTodos = removeTodo(this.state.todos, id);
 		this.setState({todos: updatedTodos});
+		destroyTodo(id)
+			.then(() => this.showTempMessage('Todo Removed'))
 	}
 
 	render() {
